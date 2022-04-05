8c38b0f240bd8baf51d7a5eb8a49abf749e5b3a4
Anna An <arzumanyan.an@gmail.com>
2022-04-05 15:41:03 +0400

    fixing suntax error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-let book = { title: "Harry Potter" };
+// import { Map } from "immutable";
+// // const Map = require("immutable");
+// // const { Map } = require("immutable");
+
+// let book = Map({ title: "Harry Potter" });
+
+// // function publish(book) {
+// //   book.isPublished = true;
+// // }
+
+// // publish(book);
+
+// console.log(book);
+
+// import { Map } from "immutable";
+
+// let book = Map({ title: "Harry Potter" });
+
+// function publish(book) {
+//   return book.set("isPublished", true);
+// }
+
+// book = publish(book);
+
+// console.log(book.toJS());
+
+import { Map } from "immutable";
+
+let book = Map({ title: "Harry Potter" });
 
 function publish(book) {
-  book.isPublished = true;
+  return book.set("isPublished", true);
 }
 
-publish(book);
+book = publish(book);
 
-console.log(book);
+console.log(book.toJS());
